f522cea4f9d582a7af6258f0c72623b79faca2a9
Luiz Branco <me@luizbranco.com>
2014-01-10 19:11:16 -0200

    Add gulp and mocha

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var mocha = require("gulp-mocha");
+var batch = require("gulp-batch");
+
+gulp.task("test", function () {
+  gulp.src("test/*.js")
+    .pipe(mocha());
+});
+
+gulp.task("default", function () {
+  gulp.watch(["app.js", "lib/**", "test/**"], batch(function (events, cb) {
+    gulp.run("test", cb);
+  }));
+});
+
