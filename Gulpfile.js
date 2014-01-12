0763d7264915999dab9a72e9466df224afc5276a
Luiz Branco <me@luizbranco.com>
2014-01-11 23:57:30 -0200

    Improve auth lib

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require("gulp");
 var exec = require("gulp-exec");
+var jshint = require("gulp-jshint");
 
 gulp.task("test", function () {
-  gulp.src("").pipe(exec("npm test"));
+  gulp.src("")
+    .pipe(exec("npm test"));
+});
+
+gulp.task("lint", function () {
+  gulp.src(["app.js", "lib/*.js"])
+    .pipe(jshint());
 });
 
 gulp.task("default", function () {
