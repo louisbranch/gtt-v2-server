ca68ee2ac1a9c435a4ba1445f51f529e52f2a70b
Luiz Branco <me@luizbranco.com>
2014-01-11 22:43:25 -0200

    Add user tests

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require("gulp");
-var mocha = require("gulp-mocha");
 var batch = require("gulp-batch");
+var exec = require("gulp-exec");
 
 gulp.task("test", function () {
   gulp.src("test/*.js")
-    .pipe(mocha("--harmony"));
+    .pipe(exec("npm test"));
 });
 
 gulp.task("default", function () {
--- test/user.js
+++ test/user.js
@@ -1,7 +1,7 @@
 var app = require("../app");
 var request = require("supertest").agent(app.listen());
 
-describe("user sign up", function(){
+describe("POST /users", function(){
 
   describe("valid params", function(){
     it("returns an user token", function(done){
@@ -9,6 +9,26 @@ describe("user sign up", function(){
       .post("/v1/users?email=me@luizbranco.com&password=secret")
       .expect(200)
       .expect(/\w{32}/, done); // 32 chars token
+    });
+  });
+
+  describe("invalid email", function(){
+
+    it("returns an error", function(done){
+      request
+      .post("/v1/users?password=secret")
+      .expect(404)
+      .expect("email required", done);
+    });
+  });
+
+  describe("invalid password", function(){
+
+    it("returns an error", function(done){
+      request
+      .post("/v1/users?email=me@luizbranco.com")
+      .expect(404)
+      .expect("password required", done);
     });
   });
 
